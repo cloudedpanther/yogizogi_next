27cdd558a31636344bf15fd0fd6bdea0815145b2
cloudedpanther <rickeykoh11@gmail.com>
2024-01-11 21:26:46 +0900

    add login and logout button toggle

--- app/Header/UserMenu/index.tsx
+++ app/Header/UserMenu/index.tsx
@@ -1,12 +1,28 @@
 'use client';
 
+import Link from 'next/link';
 import { useState } from 'react';
 
 export const UserMenu = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
+  if (isLoggedIn) {
+    return (
+      <>
+        <button onClick={handleLogout}>로그아웃</button>
+      </>
+    );
+  }
+
   return (
     <>
-      <button className="btn">로그인</button>
+      <Link href="/login" className="btn">
+        로그인
+      </Link>
     </>
   );
 };
