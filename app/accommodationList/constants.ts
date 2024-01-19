f83ddd27f55917cb3163952111fc37fdc8fd0499
cloudedpanther <rickeykoh11@gmail.com>
2024-01-19 23:51:51 +0900

    add category selects

---
+++ app/accommodationList/constants.ts
@@ -0,0 +1,1 @@
+export const CategoryStyle = `w-16 h-14 rounded-lg bg-zinc-100 hover:bg-zinc-300 cursor-pointer flex justify-center items-center`;
--- app/accommodationList/page.tsx
+++ app/accommodationList/page.tsx
@@ -1,3 +1,36 @@
+import { CategoryStyle } from './constants';
+
 export default function AccommodationList() {
-  return <div className="w-full">숙소 목록</div>;
+  return (
+    <div className="w-[1024px] mx-auto">
+      <form className="mt-4">
+        <section className="flex gap-2">
+          <label htmlFor="all" className={CategoryStyle}>
+            <p>전체</p>
+          </label>
+          <label htmlFor="hotel" className={CategoryStyle}>
+            <p>호텔</p>
+          </label>
+          <label htmlFor="motel" className={CategoryStyle}>
+            <p>모텔</p>
+          </label>
+          <label htmlFor="cottage" className={CategoryStyle}>
+            <p>펜션</p>
+          </label>
+          <>
+            <input type="radio" name="category" id="all" className="hidden" />
+            <input type="radio" name="category" id="hotel" className="hidden" />
+            <input type="radio" name="category" id="motel" className="hidden" />
+            <input
+              type="radio"
+              name="category"
+              id="cottage"
+              className="hidden"
+            />
+          </>
+        </section>
+      </form>
+      <section>숙소 목록</section>
+    </div>
+  );
 }
