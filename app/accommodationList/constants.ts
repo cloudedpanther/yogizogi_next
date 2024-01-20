d1d4dd00e6d21bd839f92fb417f4fc720a0b811a
cloudedpanther <rickeykoh11@gmail.com>
2024-01-20 16:33:46 +0900

    add sort buttons

--- app/accommodationList/constants.ts
+++ app/accommodationList/constants.ts
@@ -1,1 +1,12 @@
-export const CategoryStyle = `w-16 h-14 rounded-lg bg-zinc-100 hover:bg-zinc-300 cursor-pointer flex justify-center items-center`;
+export const FormSectionStyle =
+  'flex flex-col gap-2 border-2 rounded-lg p-4 relative';
+
+export const FormSectionLegendStyle =
+  'text-zinc-400 text-xs font-bold ml-1 absolute bg-white -top-2 -left-2';
+
+const BasicButtonStyle =
+  'h-14 rounded-lg bg-white hover:bg-zinc-100 shadow font-bold cursor-pointer flex justify-center items-center has-[:checked]:border-[3px] has-[:checked]:hover:bg-white';
+
+export const CategoryStyle = `${BasicButtonStyle} w-16 has-[:checked]:text-blue-500 has-[:checked]:border-blue-400`;
+
+export const SortStyle = `${BasicButtonStyle} w-32 has-[:checked]:text-amber-500 has-[:checked]:border-amber-400`;
--- app/accommodationList/page.tsx
+++ app/accommodationList/page.tsx
@@ -1,36 +1,95 @@
-import { CategoryStyle } from './constants';
+import {
+  CategoryStyle,
+  FormSectionLegendStyle,
+  FormSectionStyle,
+  SortStyle,
+} from './constants';
 
 export default function AccommodationList() {
   return (
-    <div className="w-[1024px] mx-auto">
-      <form className="mt-4">
-        <section className="flex gap-2">
-          <label htmlFor="all" className={CategoryStyle}>
-            <p>전체</p>
-          </label>
-          <label htmlFor="hotel" className={CategoryStyle}>
-            <p>호텔</p>
-          </label>
-          <label htmlFor="motel" className={CategoryStyle}>
-            <p>모텔</p>
-          </label>
-          <label htmlFor="cottage" className={CategoryStyle}>
-            <p>펜션</p>
-          </label>
-          <>
-            <input type="radio" name="category" id="all" className="hidden" />
-            <input type="radio" name="category" id="hotel" className="hidden" />
-            <input type="radio" name="category" id="motel" className="hidden" />
-            <input
-              type="radio"
-              name="category"
-              id="cottage"
-              className="hidden"
-            />
-          </>
+    <div className="w-[1024px] mx-auto flex flex-col gap-4">
+      {/* 상세 검색 */}
+      <form className="mt-4 shadow rounded-xl p-4 grid grid-cols-2 gap-2">
+        <section className={FormSectionStyle}>
+          <p className={FormSectionLegendStyle}>카테고리</p>
+          <div className="flex gap-2">
+            <label htmlFor="all" className={CategoryStyle}>
+              <p>전체</p>
+              <input
+                type="radio"
+                name="category"
+                id="all"
+                className="hidden"
+                checked
+              />
+            </label>
+            <label htmlFor="hotel" className={CategoryStyle}>
+              <p>호텔</p>
+              <input
+                type="radio"
+                name="category"
+                id="hotel"
+                className="hidden"
+              />
+            </label>
+            <label htmlFor="motel" className={CategoryStyle}>
+              <p>모텔</p>
+              <input
+                type="radio"
+                name="category"
+                id="motel"
+                className="hidden"
+              />
+            </label>
+            <label htmlFor="cottage" className={CategoryStyle}>
+              <p>펜션</p>
+              <input
+                type="radio"
+                name="category"
+                id="cottage"
+                className="hidden"
+              />
+            </label>
+          </div>
+        </section>
+
+        <section className={FormSectionStyle}>
+          <p className={FormSectionLegendStyle}>정렬</p>
+          <div className="flex gap-2">
+            <label htmlFor="rate" className={SortStyle}>
+              <p>별점순</p>
+              <input
+                type="radio"
+                name="sort"
+                id="rate"
+                className="hidden"
+                checked
+              />
+            </label>
+            <label htmlFor="lowPrice" className={SortStyle}>
+              <p>낮은 가격순</p>
+              <input
+                type="radio"
+                name="sort"
+                id="lowPrice"
+                className="hidden"
+              />
+            </label>
+            <label htmlFor="highPrice" className={SortStyle}>
+              <p>높은 가격순</p>
+              <input
+                type="radio"
+                name="sort"
+                id="highPrice"
+                className="hidden"
+              />
+            </label>
+          </div>
         </section>
       </form>
-      <section>숙소 목록</section>
+
+      {/* 숙소 목록 */}
+      <section className="w-full min-h-[500px] shadow rounded-xl"></section>
     </div>
   );
 }
