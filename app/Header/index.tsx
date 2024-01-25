0f5bc8b3c9e271edebb2485405326ba2b5658cbc
cloudedpanther <rickeykoh11@gmail.com>
2024-01-25 21:50:21 +0900

    add background color to header

--- app/Header/index.tsx
+++ app/Header/index.tsx
@@ -3,7 +3,7 @@ import UserMenu from './UserMenu';
 
 export default function Header() {
   return (
-    <nav className="w-full fixed flex justify-between items-center pt-2 pb-3 px-16 shadow z-10">
+    <nav className="w-full fixed flex justify-between items-center pt-2 pb-3 px-16 shadow z-10 bg-white">
       <Link href="/">
         <h1 className="font-bold text-xl text-gray-700">YogiZogi</h1>
       </Link>
