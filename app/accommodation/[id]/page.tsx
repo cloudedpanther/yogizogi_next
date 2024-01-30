c82d9835d9338ec600ed210cc450df708c2f8230
cloudedpanther <rickeykoh11@gmail.com>
2024-01-30 22:17:51 +0900

    add image section to accommodation detail page

--- app/accommodation/[id]/page.tsx
+++ app/accommodation/[id]/page.tsx
@@ -1,3 +1,38 @@
+import Image from 'next/image';
+import placeholder from '/public/images/placeholder.png';
+
 export default function Accommodation({ params }: { params: { id: string } }) {
-  return <div></div>;
+  return (
+    <div className="w-full flex flex-col items-center mt-10">
+      <section className="w-[1024px] h-[456px] grid grid-cols-3 grid-rows-2 gap-2">
+        <article className="col-span-2 row-span-2 relative">
+          <Image
+            src={placeholder}
+            alt="사진"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </article>
+        <article className="relative">
+          <Image
+            src={placeholder}
+            alt="사진"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </article>
+        <article className="relative">
+          <Image
+            src={placeholder}
+            alt="사진"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </article>
+      </section>
+    </div>
+  );
 }
